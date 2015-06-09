dd3f79cd64a211eb8c8a4c60646c5297dc60af60
Andrew Hyder <hyd415@gmail.com>
2015-06-09 15:35:27 -0700

    Reconfigured to run with gunicorn

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import json
 import os
 import datetime
+import logging
 from httplib2 import Http
 
 from oauth2client.client import SignedJwtAssertionCredentials
@@ -8,17 +9,23 @@ from apiclient.discovery import build
 
 from flask import Flask
 
+# Logging Setup
+logging.basicConfig(level=logging.INFO)
+
+
 # Config
 app = Flask(__name__)
+
 
 # Variables
 GOOGLE_ANALYTICS_PROFILE_ID = "41226190"
 GOOGLE_SERVICE_ACCOUNT_EMAIL = os.environ["GOOGLE_SERVICE_ACCOUNT_EMAIL"]
 GOOGLE_SERVICE_ACCOUNT_SECRET_KEY = os.environ["GOOGLE_SERVICE_ACCOUNT_SECRET_KEY"]
 
-
-def login_to_google_analytics_service():
-    ''' Login to the Google Analytics Service '''
+#
+# Runs when server starts
+#
+def login_to_google_analytics():
     credentials = SignedJwtAssertionCredentials(GOOGLE_SERVICE_ACCOUNT_EMAIL, GOOGLE_SERVICE_ACCOUNT_SECRET_KEY,
     'https://www.googleapis.com/auth/analytics.readonly')
     http = Http()
@@ -26,7 +33,12 @@ def login_to_google_analytics_service():
     service = build("analytics", "v3", http=http)
     return service
 
+service = login_to_google_analytics()
 
+
+#
+# Functions used in Routes
+#
 def get_clicked_issue_urls(service, id):
     ''' Get the each issue and the total times its been clicked in desc order '''
     results = service.data().ga().get(
@@ -56,7 +68,9 @@ def cleanup_clicked_issues(results):
 
     return clicked_issues
 
-
+#
+# Routes
+#
 @app.route("/")
 def index():
     results = get_clicked_issue_urls(service, GOOGLE_ANALYTICS_PROFILE_ID)
@@ -65,6 +79,4 @@ def index():
 
 
 if __name__ == '__main__':
-    service = login_to_google_analytics_service()
-
     app.run(debug=True)
