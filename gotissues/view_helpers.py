2808ff24261115be319f7796dd0c57f19150c2a4
Eunice Emefa Kokor <eek2138@columbia.edu>
2015-07-07 17:03:17 -0700

    added a way to fetch titles from issues db

--- gotissues/view_helpers.py
+++ gotissues/view_helpers.py
@@ -39,6 +39,41 @@ def get_top_acivity(db):
             activities[row["activity_type"]] += 1
     return activities
 
+def get_info_activity(db):
+  '''Get top activity types and their urls'''
+  db.execute(''' SELECT activity_type,issue_url FROM activity ''')
+  results = db.fetchall()
+
+  activities = {}
+
+  for row in results:
+    if row["activity_type"] not in activities.keys():
+      activities[row["activity_type"]] = []
+      activities[row["activity_type"]].append(row["issue_url"])
+    else:
+      activities[row["activity_type"]].append(row["issue_url"])
+  return activities
+
+def get_title_info(db, activities):
+  title_dict = {}
+  for key,val in activities.iteritems():
+    for url in activities[key]:
+      if key in title_dict:
+        title_dict[key].append(get_title_info_db(db, url))
+      else:
+        title_dict[key] = []
+        title_dict[key].append(get_title_info_db(db, url))
+  print str(title_dict) + "\n\n"
+  return title_dict
+
+def get_title_info_db(db, url):
+  ''' Get title info based on url'''
+  string = ''' SELECT title FROM issues WHERE html_url=\'%s\'''' % (url)
+  db.execute(string)
+  result = db.fetchone()
+
+  return result
+
 def get_all_activity(db):
   ''' Get all the activity '''
   db.execute(''' SELECT * FROM activity ORDER BY activity_type''')
