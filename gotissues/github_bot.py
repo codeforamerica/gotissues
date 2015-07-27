1a82719ecbbc2d1a51c24296d49e95e7ffec7a8b
Andrew Hyder <hyd415@gmail.com>
2015-07-27 11:45:53 -0700

    Filter on help wanted and nongov issues

--- gotissues/github_bot.py
+++ gotissues/github_bot.py
@@ -28,6 +28,58 @@ def github_html_url_to_api(url):
     else:
         return None
 
+
+#
+# Fetch urls that we want to ping
+#
+def get_urls(db, url=None):
+  ''' Search the issues table for specific issues we want to ping, Testing w/ fake added issue'''
+  #testing if url is not none
+  if url:
+    q = ''' SELECT html_url,clicks,view_sources,created_at FROM issues WHERE html_url=\'%s\'''' % (url)
+  else:
+    q = ''' SELECT html_url,clicks,view_sources,created_at,labels FROM issues WHERE state='open' ORDER BY created_at ASC'''
+
+  db.execute(q)
+  results = db.fetchall()
+
+  # Only return help wanted labels
+  hw_issues = []
+  for issue in results:
+    labels = issue.get('labels')
+    if labels:
+      for label in labels:
+        if label["name"] == "help wanted":
+          hw_issues.append(issue)
+
+  # Filter out government issues
+  filtered_issues = []
+  for issue in hw_issues:
+    dont_add_flag = False
+    for gov_github in ["https://github.com/presidential-innovation-fellows", "https://github.com/uscensusbureau", "https://github.com/chicago", "https://github.com/GSA", "https://github.com/project-open-data", "https://github.com/datasf","https://github.com/18f"]:
+      if issue["html_url"].startswith(gov_github):
+        dont_add_flag = True
+    if not dont_add_flag:
+      filtered_issues.append(issue)
+
+  return filtered_issues
+
+#
+# Write the urls that we posted to a db
+#
+def write_pinged_to_db(ping, db):
+  q = '''SELECT * FROM pinged_issues WHERE html_url = %(html_url)s '''
+
+  db.execute(q, {"html_url": ping["html_url"]})
+  exists = db.fetchone()
+
+  if not exists:
+    q = ''' INSERT INTO pinged_issues (html_url, status)
+        VALUES ( %(html_url)s, %(status)s) '''
+
+  db.execute(q, {"html_url":ping["html_url"], "status":ping["status"]})
+
+
 def post_on_github(url, body=None, headers=None):
   ''' Post either a specific message to the brigades or a generic one based on if there are sources or not '''
   print "Requesting %s" % (url["html_url"])
@@ -61,34 +113,20 @@ def post_on_github(url, body=None, headers=None):
     print "Error. Could not post to invalid url: %s" % (url["html_url"])
     return "error"
 
-#
-# Fetch urls that we want to ping
-#
-def get_urls(db, url=None):
-  ''' Search the issues table for specific issues we want to ping, Testing w/ fake added issue'''
-  # this is when we're testing w/ a test issue
-  if url:
-    q = ''' SELECT html_url,clicks,view_sources,created_at FROM issues WHERE html_url=\'%s\'''' % (url)
-  else:
-    q = ''' SELECT html_url,clicks,view_sources,created_at FROM issues WHERE state='open' ORDER BY created_at ASC'''
-
-  db.execute(q)
-  results = db.fetchall()
-  return results
 
 #
 # Check if we have already pinged an issue
 #
 def check_pinged(ping, db):
-  q = '''SELECT * FROM pinged_issues '''
+  q = '''SELECT * FROM pinged_issues WHERE html_url = %(html_url)s '''
 
   db.execute(q, {"html_url": ping["html_url"]})
   exists = db.fetchone()
 
   if exists:
-    return False
-  else:
     return True
+  else:
+    return False
 
 #
 # Write the urls that we posted to a db
@@ -120,11 +158,11 @@ def run_civic_bot(dailyupdate = None):
         new_url_list = []
         
         for url in url_list:
-          if check_pinged(url, db):
+          if not check_pinged(url, db):
             new_url_list.append(url)
 
-        if len(new_url_list) > 0:
-          url = random.choice(new_url_list)
+        if new_url_list:
+          url = new_url_list[0]
           print "We are about to post on %s.\nLast Updated: %s" % (url["html_url"], url["created_at"])
           
           ping = {
@@ -153,5 +191,3 @@ def run_civic_bot(dailyupdate = None):
               else:
                 print "Please re-enter a valid character\n"
                 response = None
-
-#run_civic_bot()
